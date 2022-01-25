457111f36d0c79b6bdc09221805e5c509ef33a59
Anu Anusha <97090093+AnushaV2203@users.noreply.github.com>
2022-01-25 17:08:16 +0000

    p5debug completed

---
+++ p5original.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void cmp(int a,int b,int c,int*large)
+{
+  if(a>b)
+  {
+    if(a+b)
+    {
+      *large=a;
+    }
+    else
+    {
+      *large=c;
+    }
+  }
+  else
+{
+  if(b>c)
+  {
+    *large=b;
+  }
+  else
+  {
+    *large=c;
+  }
+}
+}
+
+
+
+void input(int *a,int *b,int *c)
+{
+  printf("enter any three numbers\n");
+  scanf("%d,%d,%d",a,b,c);
+}
+int main()
+{
+  int x,y,z,large;
+  input(&x,&y,&z);
+  cmp(x,y,z,&large);
+  printf("the large number is %d",large);
+  return 0;
+}
